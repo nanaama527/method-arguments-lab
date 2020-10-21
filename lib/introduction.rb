e4944b4b9fdcb1f273af09feef427abb976098cb
nanaama527 <72888369+nanaama527@users.noreply.github.com>
2020-10-21 19:36:31 -0400

    Update introduction.rb

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,7 @@ end
 introduction("Josh")
 
 def introduction_with_language(name, language)
-    puts "Hi, my name is #{name} and I am learning to progrom in #{language}."
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end   
 
+introduction_with_language("Dan", "Ember.js")
